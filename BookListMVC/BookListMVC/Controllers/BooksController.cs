e428b43e46004725c3215324a7690c3e5e7efcf6
JSkorups <skorupski.jakub@gmail.com>
2021-03-04 22:23:53 +0100

    Adding files

---
+++ BookListMVC/BookListMVC/Controllers/BooksController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookListMVC.Controllers
+{
+    public class BooksController : Controller
+    {
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
