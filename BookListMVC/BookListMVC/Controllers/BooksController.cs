6af4ff54557e62d5c95d4ac22130a53d27a52007
JSkorups <skorupski.jakub@gmail.com>
2021-03-06 23:57:56 +0100

    Upsert post and delete

--- BookListMVC/BookListMVC/Controllers/BooksController.cs
+++ BookListMVC/BookListMVC/Controllers/BooksController.cs
@@ -34,6 +34,31 @@ namespace BookListMVC.Controllers
             }
             Book = _db.Books.FirstOrDefault(i => i.Id == id);
             return View();
+            if (Book == null)
+            {
+                return NotFound();
+            }
+            return View(Book);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(/*Book book*/)
+        {
+            if (ModelState.IsValid)
+            {
+                if (Book.Id == 0)
+                {
+                    //create
+                    _db.Books.Add(Book);
+                }
+                else
+                {
+                    _db.Books.Update(Book);
+                }
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(Book);
         }
 
         #region API Calls
